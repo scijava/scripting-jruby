f86be9c0272bc87eecf26d04e0561ab2e07ceba7
Mark Hiner <hinerm@gmail.com>
2014-08-20 15:35:25 -0500

    Create new templates
    Added new sample Ruby scripts based off the examples in
    http://imagej.net/JRuby_Scripting

---
+++ src/main/resources/script-templates/Ruby/Invert_image.rb
@@ -0,0 +1,13 @@
+# @ImagePlus i
+# A basic script that inverts the current image.
+# NB: this requires that an image is already open.
+# The ImagePlus is populated automatically via the
+# scripting framework.
+# We explicitly import the java ij.ImagePlus class,
+# but this may not be necessary if you already have
+# an imagej.rb required into this script.
+java_import "ij.ImagePlus"
+
+# Invert the image and redraw it
+$i.getProcessor.invert
+$i.updateAndDraw
---
+++ src/main/resources/script-templates/Ruby/Plasma_Cloud.rb
@@ -0,0 +1,17 @@
+# Manually create and display a color ImagePlus
+java_import "ij.process.ColorProcessor"
+java_import "ij.ImagePlus"
+
+w = 800
+h = 600
+
+# Create an ImagePlus
+cp = ColorProcessor.new(w, h)
+i = ij.ImagePlus.new "Plasma Cloud", cp
+
+# Set Image's pixels
+pixels = cp.getPixels;
+java.util.Arrays.fill(pixels, 0, w*h/2 - 1, 0x0000FF00)
+
+# Display the image
+i.show
